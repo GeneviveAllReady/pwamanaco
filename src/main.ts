d4e67f92dfa8e2f95f7420628ee73b5abfa23ed5
Nataly <nataly.casana@uab.edu.bo>
2025-09-18 21:41:37 -0400

    primer commit PWA

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
+
+bootstrapApplication(App, {
+  providers: [
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+    }),
+  ],
+});
